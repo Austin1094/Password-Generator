49eaf358c4f0d0217e81e37f78b6c8f1ca1f42eb
Austin Adams <austinadams@Austins-MacBook-Pro.local>
2023-03-13 21:46:52 -0400

    Created password generator with configuration options.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,48 @@
 // Assignment code here
+//Catergorizes characters.
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+var numericChars = "0123456789";
 
+function generatePassword() {
+  
+  let password = '';
+//Asks user to input the desired length of password.
+  const passwordLength = prompt("How long do you want your password to be? (8-128 characters)");
+//Asks again to input a new desired length if user didn't put in a number between 8 and 128.
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Oh no! Invalid password length! Please enter a number between 8 and 128.");
+    return '';
+  }
+//Asks user a series of questions regarding what they wish to be added.
+  const includeLowercase = confirm("Do you wish to include lowercase characters?");
+  const includeUppercase = confirm("Do you wish to include uppercase characters?");
+  const includeNumeric = confirm("Do you wish to include numeric characters?");
+  const includeSpecial = confirm("Do you wish to include special characters?");
+
+  let possibleChars = '';
+  if (includeLowercase) {
+    possibleChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    possibleChars += uppercaseChars;
+  }
+  if (includeNumeric) {
+    possibleChars += numericChars;
+  }
+  if (includeSpecial) {
+    possibleChars += specialChars;
+  }
+
+
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * possibleChars.length);
+    password += possibleChars[randomIndex];
+  }
+
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Password Generator Starter Code
+A password generator that begins by asking a series of questions to allow a random password to be generated using the users preferences.
+
+![Example of a random password generated](./image/Image%203-13-23%20at%208.41%20PM.jpg)
